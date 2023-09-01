ee1ab9b33429acd204fc43391555d4d9588eb2fd
ekayaint <ekayaint@gmail.com>
2023-09-01 17:12:45 +0300

    Initial Commit

---
+++ SwiftUI-GuessTheFlag-Sample/SwiftUI_GuessTheFlag_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GuessTheFlag_SampleApp.swift
+//  SwiftUI-GuessTheFlag-Sample
+//
+//  Created by ekayaint on 1.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GuessTheFlag_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
