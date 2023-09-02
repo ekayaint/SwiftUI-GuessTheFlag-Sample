9ae08f089e9915dfcd44c05ff470b972b973a84e
ekayaint <ekayaint@gmail.com>
2023-09-02 14:28:32 +0300

    Added assets

--- SwiftUI-GuessTheFlag-Sample/ContentView.swift
+++ SwiftUI-GuessTheFlag-Sample/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            VStack {
+              Text("Tap the flag of")
+              Text(countries[correctAnswer])
+            }  //: VStack
+            
+            ForEach(0..<3) {number in
+                Button {
+                    //flas was tapped
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                }
+            }
+        } //: VStack
+         
     }
 }
 
