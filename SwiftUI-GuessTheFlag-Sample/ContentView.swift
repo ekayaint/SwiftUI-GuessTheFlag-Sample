d8bd32f4e9a742edf2bf0449203a73b6ed4d804f
ekayaint <ekayaint@gmail.com>
2023-09-03 17:37:25 +0300

    Finished

--- SwiftUI-GuessTheFlag-Sample/ContentView.swift
+++ SwiftUI-GuessTheFlag-Sample/ContentView.swift
@@ -8,25 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
             VStack {
-              Text("Tap the flag of")
-              Text(countries[correctAnswer])
-            }  //: VStack
-            
-            ForEach(0..<3) {number in
-                Button {
-                    //flas was tapped
-                } label: {
-                    Image(countries[number])
-                        .renderingMode(.original)
-                }
+                Text("Guess the flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                      Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                      Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    } //: VStack
+                    
+                    
+                    ForEach(0..<3) {number in
+                        Button {
+                            if number == correctAnswer {
+                                scoreTitle = "Correct"
+                            } else {
+                                scoreTitle = "Wrong"
+                            }
+                            showingScore = true
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
+                } //: VStack
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                Text("Score: ???")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
+            } //: VStack
+            .padding()
+        } //: ZStack
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue") {
+                countries.shuffle()
+                correctAnswer = Int.random(in: 0...2)
             }
-        } //: VStack
+        } message: {
+            Text("Your score is ???")
+        }
          
     }
 }
